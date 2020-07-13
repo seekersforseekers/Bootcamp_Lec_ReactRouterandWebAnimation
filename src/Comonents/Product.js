72872ded75a5348c11a4954389e9e4cfeb659576
fakhraaslam <fakhraaslam1221@gmail.com>
2020-07-13 17:13:52 +0500

    z

--- src/Comonents/Product.js
+++ src/Comonents/Product.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Shoes from './Shoes.json';
-import {Link} from 'react-router-dom';
+import {Link ,Outlet} from 'react-router-dom';
 function Product(){
     return(
 <div>
-  <div className="Product">
+    <Outlet>
+    <div className="Product">
+
       {Object.keys(Shoes).map(keyName=> {
           const shoe=Shoes[keyName];
           return( <Link key={keyName} className="link"  to={`/product/${keyName}`}> <h2>{shoe.name}</h2><img src={shoe.img} height={250}/></Link>)})  }
     
-  </div>
+    
+    </div>
+    </Outlet>
 </div>
 
     );  
