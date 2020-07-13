8215e24fecd1a98f47622e284dffd1414c8724ea
fakhraaslam <fakhraaslam1221@gmail.com>
2020-07-13 09:59:53 +0500

    d

--- src/Comonents/Product.js
+++ src/Comonents/Product.js
@@ -7,11 +7,13 @@ function Product(){
   <div className="Product">
       {Object.keys(Shoes).map(keyName=> {
           const shoe=Shoes[keyName];
-          return( <Link key={keyName} className="link"  to={' /product/${keyName}'}> <h2>{shoe.name}</h2><img src={shoe.img} height={250}/></Link>)})  }
+          return( <Link key={keyName} className="link"  to={`/product/${keyName}`}> <h2>{shoe.name}</h2><img src={shoe.img} height={250}/></Link>)})  }
     
   </div>
 </div>
-    );                                              
+
+    );  
+                                          
 }
 
 export default Product;
--- src/Comonents/ProductItem.js
+++ src/Comonents/ProductItem.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import Shoes from './Shoes.json';
 function ProductItem(){
+  const {id}=useParams();
+  //const shoe=Shoes[id];
+  console.log(id)
     return(
 <div>
    <h1>hello prodocut item are here</h1>
